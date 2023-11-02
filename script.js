3d9cb2bb77dceb2ae674bf3e7041c2ea449a3a96
MenyePy <menyeyt@gmail.com>
2023-11-02 12:32:34 +0200

    Added GUI

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const stat = document.querySelector('.status');
 let user = 0;
 let computer = 0;
 let winScore = 5;
+let play = true;
 
 function getComputerChoice(){
     let list = ["rock", "paper", "scissors"];
     return list[Math.floor(Math.random()*3)]; 
+}
+
+function restart(){
+    play = true;
+    user = 0;
+    computer = 0;
 }
 
 function playRound (playerselection, computerselection){
@@ -16,37 +23,38 @@ function playRound (playerselection, computerselection){
     win = 0;
     if (playersel == "rock") {
         if (computerselection == "paper"){
-            resultDiv.textContent = "You Lose. Paper covers Rock.";
+            resultDiv.innerHTML = "<div style='color:red'>Lose.</div> Paper covers Rock.";
             win = -1;
         } else if (computerselection == "rock") {
-            resultDiv.textContent = "It's a tie";
+            resultDiv.innerHTML = "<div style='color:grey'>Tie</div> It's a tie";
         } else {
-            resultDiv.textContent = "You WIN! Rock smashes scissors!";
+            resultDiv.innerHTML = "<div style='color:green'>WIN!</div> Rock smashes scissors!";
             win = 1;
         }
     }
     if (playersel == "paper") {
         if (computerselection == "scissors"){
-            resultDiv.textContent = "You Lose. Scissors shreds Paper.";
+            resultDiv.innerHTML = "<div style='color:red'>Lose.</div> Scissors shreds Paper.";
             win = -1;
         } else if (computerselection == "paper") {
-            resultDiv.textContent = "It's a tie";
+            resultDiv.innerHTML = "<div style='color:grey'>Tie</div> It's a tie";
         } else {
-            resultDiv.textContent = "You WIN! Paper covers rock!";
+            resultDiv.innerHTML = "<div style='color:green'>WIN!</div> Paper covers rock!";
             win = 1;
         }
     }
     if (playersel == "scissors") {
         if (computerselection == "rock"){
-            resultDiv.textContent = "You Lose. Rock smashes scissors.";
+            resultDiv.innerHTML = "<div style='color:red'>Lose.</div> Rock smashes scissors.";
             win = -1;
         } else if (computerselection == "scissors") {
-            resultDiv.textContent = "It's a tie";
+            resultDiv.innerHTML = "<div style='color:grey'>Tie</div> It's a tie";
         } else {
-            resultDiv.textContent = "You WIN! Scissors shred paper!";
+            resultDiv.innerHTML = "<div style='color:green'>WIN!</div> Scissors shred paper!";
             win = 1;
         }
     }
+    stat.textContent = "Game in progress";
     updateScores(win);
     game();
 }
@@ -63,34 +71,47 @@ function updateScores(w){
 
 function game(){
     if (user == winScore){
-        stat.textContent = "The user WINS!";
-        user = 0;
-        computer = 0;
+        stat.textContent = "You Win! Game over.";
+        play = false;
     } else if (computer == winScore) {
-        stat.textContent = "The computer WINS!";
-        user = 0;
-        computer = 0;
-    } else {
-        if (user > 0 || computer > 0) {
-            stat.textContent = "Game in progress";
-        } else {
-            stat.textContent = "Not playing.";
-        }
+        stat.textContent = "You Lose. Game over.";
+        play = false;
     }
 }
 
 const rockBtn = document.querySelector('.rock');
 const scissorBtn = document.querySelector('.scissors');
 const paperBtn = document.querySelector('.paper');
+const restartBtn = document.querySelector('.restartBtn');
 
 rockBtn.addEventListener('click', () => {
-    playRound("rock", getComputerChoice());
+    if (play){
+        playRound("rock", getComputerChoice());
+    } else {
+        alert ("Click the restart button to play again");
+    }
 });
 
 scissorBtn.addEventListener('click', () => {
-    playRound("scissors", getComputerChoice());
+    if (play){
+        playRound("scissors", getComputerChoice());
+    } else {
+        alert ("Click the restart button to play again");
+    }
 });
 
 paperBtn.addEventListener('click', () => {
-    playRound("paper", getComputerChoice());
+    if (play){
+        playRound("paper", getComputerChoice());
+    } else {
+        alert ("Click the restart button to play again");
+    }
+});
+
+restartBtn.addEventListener('click', () => {
+    restart();
+    userScore.firstElementChild.textContent = user;
+    computerScore.firstElementChild.textContent = computer;
+    resultDiv.textContent = "Click to play!";
+    stat.textContent = "Not playing"
 });
